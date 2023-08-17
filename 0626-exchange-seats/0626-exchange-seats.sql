7cd185bade5f62b90211857ebeb6ee0ab6fae2e6
Wonjae Kim <wonjaek36@protonmail.com>
2023-08-18 02:45:47 +0900

    Time: 794 ms (52.79%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,5 +1,19 @@
 # Write your MySQL query statement below
 
+# USE CASE END
+SELECT 
+  CASE
+    WHEN id % 2 = 1 AND id = (SELECT COUNT(*) FROM Seat) THEN id
+    WHEN id % 2 = 0 THEN id-1
+    ELSE id+1
+  END AS id,
+  student
+
+FROM Seat
+ORDER BY id
+
+
+/* FIRST IDEA
 SELECT RANK() OVER (ORDER BY id ASC) AS id, student 
 FROM (
   SELECT (id-1) AS id, student
@@ -10,3 +24,4 @@ FROM (
   FROM Seat
   WHERE mod(id, 2) = 1
 ) AS Temp
+*/
